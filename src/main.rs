90eb515e1cad6ac9b4a31229bb11cb4be468a180
enuesaa <nsrciog@gmail.com>
2023-01-20 23:36:19 +0900

    feat: clap settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(short = 'a', long = "aaa", default_value_t = String::from("aaa"))]
+    aaa: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
