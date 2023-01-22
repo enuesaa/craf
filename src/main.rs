0c7be4f6ab355de4c783fee602cec9c40928916c
enuesaa <nsrciog@gmail.com>
2023-01-23 01:43:42 +0900

    feat: define cli args

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use clap::{Parser, Subcommand};
 
-#[derive(Debug, Parser)]
+#[derive(Parser)]
+#[command(name = "craftant", bin_name = "craftant", long_about = None)]
 struct Args {
     #[command(subcommand)]
     action: Action,
 }
 
-#[derive(Debug, Subcommand)]
+#[derive(Subcommand)]
+#[command(disable_help_subcommand = true)]
 enum Action {
-   Add,
-   Remove,
+    Register {
+        #[arg(long = "name", required = true)]
+        name: String,
+    },
+    Unregister {
+        #[arg(long = "name", required = true)]
+        name: String,
+    },
+    Apply {
+        #[arg(long = "name", required = true)]
+        name: String,
+        #[arg(long = "approve", required = false, default_value_t = false)]
+        approve: bool,
+    },
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{:?}", args);
+    let _args = Args::parse();
 }
