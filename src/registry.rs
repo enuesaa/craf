6ef96a30ccfd8e06f3fbc4607b7ba9442aa57bec
enuesaa <nsrciog@gmail.com>
2023-01-26 00:52:46 +0900

    feat: use`dirs` crate to create registry in home dir

--- src/registry.rs
+++ src/registry.rs
@@ -1,7 +1,12 @@
 use std::fs;
 use std::io::Result;
 
+use dirs::home_dir;
+
 pub fn create_registry() -> Result<()> {
-  fs::create_dir(".craftant")?;
+  let mut registry_path = home_dir().unwrap();
+  registry_path.push(".craftant");
+  println!("{:?}", registry_path);
+  fs::create_dir(registry_path)?;
   Ok(())
 }
