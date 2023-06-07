3ea50ed1ca97a9aff433184c505db4232705f782
enuesaa <nsrciog@gmail.com>
2023-06-08 00:55:57 +0900

    feat: logger を仕込もうと思ったが難しそうなのでやめた

--- src/run.rs
+++ src/run.rs
@@ -2,15 +2,18 @@ use std::process::Command;
 use std::io::Result;
 
 pub fn run() -> Result<()> {
-    let mut child = Command::new("node")
-        // .args(["--version"])
+    let child = Command::new("node")
         .spawn()
         .unwrap();
-    let a = child.wait().unwrap();
-    println!("{:?}", a);
-    // let stdin = child.stdin.take().unwrap();
-    // let stdout = child.stdout.take().unwrap();
-    // println!("{:?}", stdin);
-    // println!("{:?}", stdout);
+
+    let res = child
+        .wait_with_output()
+        .unwrap();
+    println!("{:?}", res);
+
+    // https://keens.github.io/blog/2016/12/02/rustnopurosesu/
+    // https://www.howtosolutions.net/2021/10/rust-creating-simple-terminal-interactive-input-prompt-app/
+
+
     Ok(())
 }
