8460ab2be6960ae6590485942991774681547487
enuesaa <nsrciog@gmail.com>
2023-06-07 23:54:58 +0900

    feat: コマンドを実行できるようにしたはず

--- src/run.rs
+++ src/run.rs
@@ -1,5 +1,16 @@
+use std::process::Command;
 use std::io::Result;
 
 pub fn run() -> Result<()> {
+    let mut child = Command::new("node")
+        // .args(["--version"])
+        .spawn()
+        .unwrap();
+    let a = child.wait().unwrap();
+    println!("{:?}", a);
+    // let stdin = child.stdin.take().unwrap();
+    // let stdout = child.stdout.take().unwrap();
+    // println!("{:?}", stdin);
+    // println!("{:?}", stdout);
     Ok(())
 }
