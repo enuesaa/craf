161aa6534790d539c261ca66c8d6afec66624b95
enuesaa <nsrciog@gmail.com>
2023-06-09 20:39:19 +0900

    feat: 不要なコマンドを削除

--- src/apply.rs
+++
@@ -1,9 +0,0 @@
-use crate::registry::get_template_path;
-
-use std::fs;
-use std::io::Result;
-
-pub fn apply_template(name: &str) -> Result<()> {
-    fs::copy(get_template_path(name), name)?;
-    Ok(())
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use clap::{Parser, Subcommand};
 
-pub mod apply;
-pub mod registry;
 pub mod run;
 
-use crate::apply::apply_template;
-use crate::registry::{add_template, create_registry, remove_template};
 use crate::run::run;
 
 #[derive(Parser)]
@@ -18,49 +14,12 @@ struct Cli {
 #[derive(Subcommand)]
 #[command(disable_help_subcommand = true)]
 enum Action {
-    #[command(subcommand)]
-    Template(TemplateAction),
-    Apply {
-        #[arg(long = "name", required = true)]
-        name: String,
-        #[arg(long = "approve", required = false, default_value_t = false)]
-        approve: bool,
-    },
     Run {},
-}
-
-#[derive(Subcommand)]
-enum TemplateAction {
-    Add {
-        #[arg(long = "name", required = true)]
-        name: String,
-    },
-    Remove {
-        #[arg(long = "name", required = true)]
-        name: String,
-    },
-    List {},
 }
 
 fn main() {
     let cli = Cli::parse();
     match cli.action {
-        Action::Template(args) => match args {
-            TemplateAction::Add { name } => {
-                println!("template add: {:?}", name);
-                let _ = create_registry();
-                let _ = add_template(&name);
-            }
-            TemplateAction::Remove { name } => {
-                println!("template remove: {:?}", name);
-                let _ = remove_template(&name);
-            }
-            TemplateAction::List {} => {}
-        },
-        Action::Apply { name, approve } => {
-            println!("apply: {:?}, {:?}", name, approve);
-            let _ = apply_template(&name);
-        },
         Action::Run {} => {
             println!("run");
             let _ = run();
