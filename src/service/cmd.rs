b84a83d59e27fb2268189eae478d684efc315e26
nua <enuesaa@gmail.com>
2024-05-21 01:52:17 +0900

    refactor(registry): change registry place to `~/.craf`

--- src/service/cmd.rs
+++ src/service/cmd.rs
@@ -23,7 +23,7 @@ pub struct CmdService<R: FilesRepository> {
 }
 impl<R: FilesRepository> CmdService<R> {
     fn get_registry_path(&self) -> String {
-        String::from(".crafant")
+        String::from(".craf")
     }
 
     fn is_registry_exist(&self) -> bool {
